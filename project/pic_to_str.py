048c44521805e774c8febc50fdcc7990a6f2ee56
KengKaK <suppadej3@gmail.com>
2024-01-04 02:12:51 +0700

    change tesseract location

--- project/pic_to_str.py
+++ project/pic_to_str.py
@@ -4,7 +4,7 @@ import re
 import datetime
 time = datetime.datetime.now()
 
-pytesseract.pytesseract.tesseract_cmd = r"C:\Users\suppa\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
+pytesseract.pytesseract.tesseract_cmd = r"D:\Program Files\Tesseract-OCR\tesseract.exe"
 def image_to_str():
     Image_path = 'received_line.jpeg'
 
