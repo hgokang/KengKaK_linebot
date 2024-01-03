ba4114628554a139219a5089e45e8b7223800b31
KengKaK <suppadej3@gmail.com>
2024-01-04 02:12:24 +0700

    delete print

--- project/check_mont.py
+++ project/check_mont.py
@@ -12,10 +12,7 @@ def check_month():
         data = line.strip().split()
         date_str = data[-2]  # ดึงวันที่จากข้อมูล
         date = datetime.datetime.strptime(date_str, '%d/%m/%y')
-        print(date)
-        print(line)
         delta = current_date - date
-        print(delta.days)
         if delta.days <= 30:
             print('pass')
             new_lines.append(line)
